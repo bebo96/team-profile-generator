ae4b8d3126d9b424b589c465c7480bfb76ae1ae5
bebo <ibrahimimran96@gmail.com>
2022-10-06 12:19:06 -0400

    Added jest and Employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require('../lib/Employee');
+
+test('Creates new employee', () => {
+    const employee = new Employee('Learin Tino', 4444, 'learintino@gmail.com');
+
+    expect(employee.name).toBe("Learin Tino");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+})
+
+test('Tests all functions within Class Employee', () => {
+    const employee = new Employee('Learin Tino', 4444, 'learintino@gmail.com');
+
+    expect(employee.getName()).toBe(employee.name);
+
+    expect(employee.getId()).toBe(employee.id);
+    
+    expect(employee.getEmail()).toBe(employee.email);
+    
+    expect(employee.getRole()).toBe('Employee');
+
+})
